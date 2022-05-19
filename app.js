01e2c4cc4126d94fdf41368e50c3193878911b5b
Agels <ardhianagilr@gmail.com>
2022-05-20 00:20:59 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+import express from 'express';
+
+const app =  express();
+import fetch from 'node-fetch';
+
+const port = 3000;
+
+app.set('view engine','ejs');
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+
+app.get('/users', (req, res) => {
+
+    res.render('users')
+});
+
+app.post('/users', async (req, res) => {
+     const response = await fetch('https://fakestoreapi.com/Users');
+     const data = await response.json();
+
+  res.send(data)
+
+})
+
+app.get('/products', (req, res) => {
+    res.render('products')
+});
+
+
+app.post('/products', async (req, res) => {
+    const response = await fetch('https://fakestoreapi.com/Products?limit=10');
+    const data = await response.json();
+
+ res.send(data)
+
+})
+
+
+
+app.listen(port, () => console.log(`running at : localhost:${port}`))
+
